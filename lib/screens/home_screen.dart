66d2c3ccc6ed24c8cb0ed4e0641ff8d228d1f737
Syb10 <sybillepradella@web.de>
2019-11-13 11:43:42 +0100

    Button in Methode

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,72 +17,9 @@ class HomeScreen extends StatelessWidget {
                 padding: const EdgeInsets.only(bottom: 16.0),
                 child: Image.asset("assets/images/Hochschule_46.jpg"),
               ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: RaisedButton.icon(
-                  label: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Text(
-                      "Kalender",
-                      style: TextStyle(
-                        fontSize: 24,
-                      ),
-                    ),
-                  ),
-                  color: Colors.blue[900],
-                  textColor: Colors.white,
-                  icon: Icon(Icons.calendar_today),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(30.0)),
-                  onPressed: () {
-                    navigate(context, KalenderScreen());
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: RaisedButton.icon(
-                  label: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Text(
-                      "Personen",
-                      style: TextStyle(
-                        fontSize: 24,
-                      ),
-                    ),
-                  ),
-                  color: Colors.blue[900],
-                  textColor: Colors.white,
-                  icon: Icon(Icons.person_pin),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(30.0)),
-                  onPressed: () {
-                    navigate(context, PersonenScreen());
-                  },
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: RaisedButton.icon(
-                  label: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Text(
-                      "Raum",
-                      style: TextStyle(
-                        fontSize: 24,
-                      ),
-                    ),
-                  ),
-                  color: Colors.blue[900],
-                  textColor: Colors.white,
-                  icon: Icon(Icons.room),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: new BorderRadius.circular(30.0)),
-                  onPressed: () {
-                    navigate(context, RaumScreen());
-                  },
-                ),
-              ),
+              buildButton(context,"Kalender",Icons.calendar_today, KalenderScreen()),
+              buildButton(context,"Personen",Icons.person_pin, PersonenScreen()),
+              buildButton(context,"Raum",Icons.room, RaumScreen()),
             ],
           ),
         ),
@@ -90,11 +27,33 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  void navigate(BuildContext context, widget) {
-    Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => widget,
-        ));
+  Padding buildButton(BuildContext context,text,icon,widget) {
+    return Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: RaisedButton.icon(
+                label: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    text,
+                    style: TextStyle(
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+                color: Colors.blue[900],
+                textColor: Colors.white,
+                icon: Icon(icon),
+                shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(30.0)),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => widget,
+                      ));
+                },
+              ),
+            );
   }
+
 }
